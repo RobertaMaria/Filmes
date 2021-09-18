9ac592eb111de6bdbdc471b40584c85552c85470
Gigaton-Furia <robertaji26@gmail.com>
2021-09-18 09:45:40 -0300

    Criada a classe AppApplication para a configuração do Koin.

---
+++ app/src/main/java/com/roberta/filmes/AppApplication.kt
@@ -0,0 +1,19 @@
+package com.roberta.filmes
+
+import android.app.Application
+import com.roberta.filmes.di.modules.appModules
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+
+class AppApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidContext(this@AppApplication)
+            modules(appModules)
+        }
+    }
+
+
+}
