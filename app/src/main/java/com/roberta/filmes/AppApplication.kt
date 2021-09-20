fa9db863050d1181d3df43032ec263a5f5bc50a0
Gigaton-Furia <robertaji26@gmail.com>
2021-09-20 00:23:06 -0300

    Refatorando código.

--- app/src/main/java/com/roberta/filmes/AppApplication.kt
+++ app/src/main/java/com/roberta/filmes/AppApplication.kt
@@ -5,6 +5,7 @@ import com.roberta.filmes.di.modules.appModules
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
+
 class AppApplication : Application() {
 
     override fun onCreate() {
