250ee14aeb0de8fa9b7988f34a25c7c40452202a
Gigaton-Furia <robertaji26@gmail.com>
2021-09-20 00:30:03 -0300

    Atualizando configuração do Retrofit.

--- app/src/main/java/com/roberta/filmes/retrofit/ConfiguraRetrofit.kt
+++ app/src/main/java/com/roberta/filmes/retrofit/ConfiguraRetrofit.kt
@@ -1,6 +1,8 @@
 package com.roberta.filmes.retrofit
 
 import com.roberta.filmes.retrofit.service.FilmesService
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -8,9 +10,15 @@ private const val BASE_URL = "https://api.themoviedb.org/3/"
 
 class ConfiguraRetrofit {
 
+    private var interceptor = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+    private val okHttpClient = OkHttpClient.Builder()
+        .addInterceptor(interceptor)
+        .build()
+
     private val retrofit =
         Retrofit.Builder()
             .baseUrl(BASE_URL)
+            .client(okHttpClient)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
 
