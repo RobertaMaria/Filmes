8a9a6552f21593c63c271f2c8a79bcf6598b109f
Gigaton-Furia <robertaji26@gmail.com>
2021-09-20 14:40:04 -0300

    Removendo atributos não usados.

--- app/src/main/java/com/roberta/filmes/model/Filmes.kt
+++ app/src/main/java/com/roberta/filmes/model/Filmes.kt
@@ -5,10 +5,6 @@ import androidx.room.*
 @Entity
 data class Filmes(
     val backdrop_path: String,
-    //@Ignore
-    //val genre_ids: List<Int> = arrayListOf(),
-    //@TypeConverters(ConversorLista::class)
-    //var genre_names: ArrayList<String>,
     @PrimaryKey
     val id: Int,
     val original_language: String,
