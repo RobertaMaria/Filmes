74a4d58737374baf9df9f6b3d2a9444c81a0721f
Gigaton-Furia <robertaji26@gmail.com>
2021-09-20 00:24:20 -0300

    Adicionada novas dependências e removidas as que não estava mas sendo usada.

--- app/src/main/java/com/roberta/filmes/di/modules/AppModules.kt
+++ app/src/main/java/com/roberta/filmes/di/modules/AppModules.kt
@@ -2,23 +2,23 @@ package com.roberta.filmes.di.modules
 
 import com.roberta.filmes.BuildConfig
 import com.roberta.filmes.adapter.ListaFilmesAdapter
+import com.roberta.filmes.dao.DetalhesFilmeDao
 import com.roberta.filmes.dao.FilmesDao
 import com.roberta.filmes.database.FilmesDataBase
+import com.roberta.filmes.repository.FilmesRepository
 import com.roberta.filmes.repository.FilmesRepositoryImp
 import com.roberta.filmes.retrofit.ConfiguraRetrofit
 import com.roberta.filmes.retrofit.FilmesPagingSource
-import com.roberta.filmes.retrofit.GithubRemoteMediator
 import com.roberta.filmes.retrofit.service.FilmesService
+import com.roberta.filmes.ui.viewmodel.DetalhesFilmesViewModel
 import com.roberta.filmes.ui.viewmodel.ListaFilmesViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
+
 val appModules = module {
     single<FilmesDataBase> {
         FilmesDataBase.getInstance(get())
-    }
-    single<FilmesPagingSource> {
-        FilmesPagingSource(service = get(), chaveApi = BuildConfig.CHAVE)
     }
     single<FilmesService> {
         ConfiguraRetrofit().getFilmesService()
@@ -26,17 +26,27 @@ val appModules = module {
     single<FilmesDao> {
         get<FilmesDataBase>().filmesDao
     }
-    single<FilmesRepositoryImp> {
-        FilmesRepositoryImp(service = get(), dao = get(), pagingSource = get())
+    single<DetalhesFilmeDao> {
+        get<FilmesDataBase>().detalhesFilmeDao
     }
-    single<GithubRemoteMediator> {
-        GithubRemoteMediator(service = get(), dataBase = get())
+    single<FilmesRepository> {
+        FilmesRepositoryImp(service = get(), detalhesDao = get(), chaveApi = get(), pagingSource = get())
     }
+    single<String> {
+        BuildConfig.CHAVE
+    }
+    single<FilmesPagingSource> {
+        FilmesPagingSource(service = get(), chaveApi = get())
+    }
+
     factory<ListaFilmesAdapter> {
         ListaFilmesAdapter()
     }
     viewModel<ListaFilmesViewModel> {
         ListaFilmesViewModel(repository = get())
+    }
+    viewModel<DetalhesFilmesViewModel> {(id : Int)->
+        DetalhesFilmesViewModel(repository = get(), filmeId = id)
     }
 
 }
