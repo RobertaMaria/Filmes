98aafd95c6402ea5e6764b83a07262490e879a04
Gigaton-Furia <robertaji26@gmail.com>
2021-09-20 00:27:54 -0300

    Deletadas classes não usadas.

--- app/src/main/java/com/roberta/filmes/retrofit/service/RetornoConfiguracaoFilmesAlpi.kt
+++
@@ -1,5 +0,0 @@
-package com.roberta.filmes.retrofit.service
-
-import com.roberta.filmes.model.ConfiguracaoFilme
-
-class RetornoConfiguracaoFilmesAlpi(val images: ConfiguracaoFilme, val change_keys: List<String>)
