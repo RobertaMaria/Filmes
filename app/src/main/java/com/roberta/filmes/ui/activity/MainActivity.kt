0aaafab643bfb3a32031b55a9f7c0992d93de1d2
Gigaton-Furia <robertaji26@gmail.com>
2021-09-20 00:36:23 -0300

    Configurado o titulo dos fragments.

--- app/src/main/java/com/roberta/filmes/ui/activity/MainActivity.kt
+++ app/src/main/java/com/roberta/filmes/ui/activity/MainActivity.kt
@@ -2,11 +2,22 @@ package com.roberta.filmes.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
 import com.roberta.filmes.R
 
 class MainActivity : AppCompatActivity() {
+
+    private val controlador by lazy {
+        findNavController(R.id.main_activity_nav_host)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        controlador.addOnDestinationChangedListener { controller, destination, arguments ->
+
+            title = destination.label
+        }
     }
 }
